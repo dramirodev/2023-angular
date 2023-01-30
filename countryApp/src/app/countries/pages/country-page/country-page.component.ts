45dac9051cd9a9d33ba8ec9ed427deb0d4665980
M. David Ramiro Barragán <drambar@innusual.es>
2023-01-30 14:27:14 +0100

    country services

--- countryApp/src/app/countries/pages/country-page/country-page.component.ts
+++ countryApp/src/app/countries/pages/country-page/country-page.component.ts
@@ -1,22 +1,35 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {switchMap} from "rxjs";
+import {Country} from "../../interfaces/country";
+import {CountriesService} from "../../services/countries.service";
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
-  styles: [
-  ]
+  styles: []
 })
-export class CountryPageComponent implements OnInit{
-  constructor(private _activatedRoute: ActivatedRoute) {
+export class CountryPageComponent implements OnInit {
+
+  public country?: Country;
+
+  constructor(private _activatedRoute: ActivatedRoute, private _countriesService: CountriesService, private _router: Router) {
 
 
   }
 
   ngOnInit(): void {
-    this._activatedRoute.params.subscribe(({id} )=> {
+    this._activatedRoute.params
+      .pipe(
+        switchMap(({id}) => this._countriesService.searchCountryByAlphaCode(id))
+      )
+      .subscribe((country) => {
+        if (!country) {
+          return this._router.navigateByUrl('');
+        }
 
-    });
-    }
+
+      });
+  }
 
 }
--- countryApp/src/app/countries/services/countries.service.ts
+++ countryApp/src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import {HttpClient} from "@angular/common/http";
-import { Injectable } from '@angular/core';
-import {Observable, of, catchError} from "rxjs";
+import {Injectable} from '@angular/core';
+import {catchError, map, Observable, of} from "rxjs";
 import {Country} from "../interfaces/country";
 
 @Injectable({
@@ -8,25 +8,34 @@ import {Country} from "../interfaces/country";
 })
 export class CountriesService {
 
-  private url: string= "https://restcountries.com/v3.1";
+  private url: string = "https://restcountries.com/v3.1";
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+  }
 
-  searchByCapital(query: string):Observable<Country[]>{
+  searchByCapital(query: string): Observable<Country[]> {
     return this.httpClient.get<Country[]>(`${this.url}/capital/${query}`).pipe(
       catchError(() => of([]))
     );
   }
 
-  searchByRegion(query: string):Observable<Country[]>{
+  searchByRegion(query: string): Observable<Country[]> {
     return this.httpClient.get<Country[]>(`${this.url}/region/${query}`).pipe(
       catchError(() => of([]))
     );
   }
 
-  searchByCountry(query: string):Observable<Country[]>{
+  searchByCountry(query: string): Observable<Country[]> {
     return this.httpClient.get<Country[]>(`${this.url}/name/${query}`).pipe(
       catchError(() => of([]))
     );
+  }
+
+  searchCountryByAlphaCode(query: string): Observable<Country | null> {
+    return this.httpClient.get<Country[]>(`${this.url}/alpha/${query}`)
+      .pipe(
+        map(countries => countries.length > 0 ? countries[0] : null),
+        catchError(() => of(null))
+      );
   }
 }
