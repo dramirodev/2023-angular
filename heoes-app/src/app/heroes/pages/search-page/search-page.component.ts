9f2f45973142f67d76ab1527cae45dc9b1daa7cf
M. David Ramiro Barragán <drambar@innusual.es>
2023-02-07 09:23:31 +0100

    start crud section

--- heoes-app/src/app/heroes/pages/search-page/search-page.component.ts
+++ heoes-app/src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {FormControl} from "@angular/forms";
+import {MatAutocompleteSelectedEvent} from "@angular/material/autocomplete";
+import {Hero} from "../../interfaces/hero.interface";
+import {HeroesService} from "../../services/heroes.service";
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class SearchPageComponent {
-public searchInput = new FormControl('');
+  public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+  public selectedHero?: Hero;
+
+  constructor(private _heroService: HeroesService) {
+  }
+
+
+  searchHero() {
+    const value: string = this.searchInput.value || '';
+
+    this._heroService.getSuggestions(value)
+      .pipe()
+      .subscribe(heroes => this.heroes = heroes);
+
+  }
+
+  onSelectedOption(event: MatAutocompleteSelectedEvent){
+    if(!event.option.value){
+      this.selectedHero = undefined;
+      return;
+    }
+
+    const hero: Hero = event.option.value;
+
+    this.searchInput.setValue(hero.superhero);
+    this.selectedHero = hero;
+  }
+
+
 }
