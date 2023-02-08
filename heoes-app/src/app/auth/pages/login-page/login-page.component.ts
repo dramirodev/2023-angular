c7afc3c54ec240e50ab3f6e3c1c18c36ce027d78
M. David Ramiro Barragán <drambar@innusual.es>
2023-02-08 14:49:09 +0100

    feat: add auth service

--- heoes-app/src/app/auth/pages/login-page/login-page.component.ts
+++ heoes-app/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styles: [],
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin() {
+    this.authService
+      .login('derfsaf', '123456')
+      .subscribe(() => this.router.navigateByUrl('/'));
+  }
+}
---
+++ heoes-app/src/app/auth/services/auth.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, map, Observable, of, tap } from 'rxjs';
+import { environments } from '../../../environments/environments';
+import { User } from '../interfaces/user.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private baseUrl = environments.baseUrl;
+  private user?: User;
+  constructor(private _httpClient: HttpClient) {}
+
+  get currentUser(): User | undefined {
+    if (!this.user) {
+      return undefined;
+    }
+    return structuredClone(this.user);
+  }
+
+  login(email: string, pass: string): Observable<User> {
+    return this._httpClient.get<User>(`${this.baseUrl}/users/1`).pipe(
+      tap(user => (this.user = user)),
+      tap(user => localStorage.setItem('token', user.id.toString()))
+    );
+  }
+
+  checkAuthenticationStatus(): Observable<boolean> | boolean {
+    if (!localStorage.getItem('token')) {
+      return false;
+    }
+
+    const token = localStorage.getItem('token');
+
+    return this._httpClient.get<User>(`${this.baseUrl}/users/1`).pipe(
+      tap(user => (this.user = user)),
+      map(user => !!user),
+      catchError(() => of(false))
+    );
+  }
+
+  logout(): void {
+    this.user = undefined;
+    localStorage.clear();
+  }
+}
